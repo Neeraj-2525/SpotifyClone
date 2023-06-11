678de617054fc842a422fdbbbfa1a1cac2a97d32
Neeraj-2525 <neerajkumara2525@gmail.com>
2023-06-11 14:30:54 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+//  ************* TO TOGGLE THE HAMBURGER AND CROSS ICON ***************
+
+function toggleIcons() {
+    const openIcon = document.getElementById("open-icon");
+    const collapseIcon = document.getElementById("collapse-icon");
+
+    openIcon.style.display = (openIcon.style.display === "none") ? "inline-block" : "none";
+    collapseIcon.style.display = (collapseIcon.style.display === "none") ? "inline-block" : "none";
+}
+
+
+
+// ***************** Open DropDown ***************** 
+
+function openDropDown() {
+    document.getElementById("onClickNavId").style.display = "block";
+}
+
+function closeDropDown() {
+    document.getElementById("onClickNavId").style.display = "none";
+}
+
+// %%%%%%%%%%%%%%%%% TO SHOW PLAY BTN %%%%%%%%%%%%%%%%%
+// const cardEle = document.getElementsByClassName("card")
+// const playBtnEle = document.getElementsByClassName("play-btn")
+// cardEle.addEventListener("click",()=>{
+//     playBtnEle.classList.toggle("play-btn-show")
+// })
